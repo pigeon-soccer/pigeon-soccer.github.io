93e4492b07a7c37e6b590ff6ba69015ac091f62c
Masato Emata (江俣 真人) <lh57.nm@gmail.com>
2020-10-25 02:55:28 +0900

    SMPフォームへのデザイン適用Javascript

--- common/js/main.js
+++ common/js/main.js
@@ -49,38 +49,6 @@ $(document).on('click','.globalNav__btn', function() {
     }, 200);
 });
 
-// 以下はJQueryのライブラリの問題で上手く動作せず
-// $(function(){
-// //    /*test
-//     console.log("function loaded")
-//     if (typeof jQuery == 'undefined') {
-//       console.log('jQuery not loaded');
-//     } else {
-//       console.log('jQuery loaded');
-//     }
-//     if($('.globalNav__btn')) {console.log("dom loaded")}
-//    */
-//     $(".globalNav__btn").on("click", function(){
-//         console.log("fired on-click") //test
-//         // ハンバーガーメニューの位置を設定
-//         var rightVal = 0;
-//         if($(this).hasClass("open")) {
-//             // 位置を移動させメニューを開いた状態にする
-//             rightVal = -700;
-//             // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-//             $(this).removeClass("open");
-//         } else {
-//             // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-//             $(this).addClass("open");
-//         }
-
-//         $(".globalNav").stop().animate({
-//             right: rightVal
-//         }, 200);
-//     });
-// });
-
-
 $(function() {
     //クリックしたときのファンクションをまとめて指定
     $('.tab > li.tab__label').click(function() {
@@ -99,89 +67,68 @@ $(function() {
 });
 
 
-// //-----SMP form-----
-// if(document.location.pathname=="/contact/"){
-//     //SMPフォームの読み込みを判別
-//     var timer = setInterval(function(){
-//         //SMPフォームの読み込みが終わったときの処理
-//         if(document.querySelector(".smpForm form td input")){
-//             //Loading非表示
-//             document.querySelector(".loadingWrap").style.display = "none";
-//             //フォームのレイアウト修正
-//             //「非表示」とつく入力項目を非表示にする
-//             //直接IDを指定してCSSで非表示にする方針と比較可
-//             var elms = document.querySelectorAll(".ss_form_title")
-//             for(var i=0; i<elms.length; i++){
-//                 if( elms[i].innerText.match(/非表示/) ){
-//                     elms[i].parentNode.style.display = "none";
-//                 }
-//             }
-//             //フォームカテゴリの修正
-//             var categoryName_address = "ご連絡先"
-//             var categoryName_detail = "お問い合わせ内容"
-//             var targetElm_selector_address = "#ss_name"
-//             var targetElm_selector_detail = "#ss_VisitorData\\.attribute34"
-//             var categoryClassName_address = "formCategory_address"
-//             var categoryClassName_detail = "formCategory_delail"
-//             function categoryTitleElement(categoryName, targetElm_selector,categoryClassName){
-//                 //フォームカテゴリ名の要素の作成
-//                 var p_elm = document.createElement('p');
-//                 p_elm.textContent = categoryName
-//                 p_elm.className = categoryClassName
-//                 $(p_elm).insertBefore(targetElm_selector);
-//                 //フォームカテゴリ名のスタイル
-//                 var categoryTitleStyle = document.querySelector("p."+categoryClassName).style;
-//                 categoryTitleStyle.textAlign = "center";
-//                 categoryTitleStyle.fontWeight = "bold";
-//                 categoryTitleStyle.fontSize = "2rem";
-//                 categoryTitleStyle.padding = "30px 0";
-//             }
-//             categoryTitleElement(categoryName_address, targetElm_selector_address, categoryClassName_address);
-//             categoryTitleElement(categoryName_detail, targetElm_selector_detail, categoryClassName_detail);
+//SMP Form 関連
+//フォームURLの場合、SMPフォームの読み込みを待ってからデザインの修正処理をする
+if (document.location.pathname == "/contact/"){
+    $(function () {
+        var set_interval_id = setInterval(findCartButton, 1000);
+        function findCartButton() {
+            var cart_button = document.querySelector(".smpForm");
+            if (cart_button) {
+                clearInterval(set_interval_id);
+                window.dataLayer.push({ "event": "find_smpForm" });
+                customizeSMPFormHTML();
+            }
+        }
+    });
+}
 
-//             //フォーム項目の修正実行
-//             document.querySelector("[name='Public::EmbeddedApplication::User_D__P__D_email']").placeholder = "例 - sample@pigeon.com"
-//             document.querySelector("[name='Public::EmbeddedApplication::User_D__P__D_name1']").placeholder = "例 - 山田"
-//             document.querySelector("[name='Public::EmbeddedApplication::User_D__P__D_VisitorData.attribute36']").placeholder = "ご入力ください"
+//SMPの埋め込みFormのHTMLを上書きするための関数
+function customizeSMPFormHTML(){
+    //読み込み中の文言を非表示化、および非表示というワードが含まれる項目を非表示化する
+    $(function () {
+        var loading_elms = document.querySelectorAll(".loadingWrap");
+        var display_none_attribute_elms = Array.from(document.querySelectorAll(".ss_form_title")).filter(function (elm) { return elm.innerText.match(/非表示/) });
+        Array.from(loading_elms).map(function (elm) { return elm.style.display = "none"; });
+        Array.from(display_none_attribute_elms).map(function (elm) { return elm.parentNode.style.display = "none"; });
+    });
 
-//             //CSSを適用しやすくするために選択肢テキストを<label>タグで囲む
-//             function setLebelTag(selector_text){
-//                 var old_nodes = document.querySelectorAll(selector_text)
-//                 var old_node = [];
-//                 var new_node = [];
-//                 var target_node = [];
-//                 for(var i=0; i<old_nodes.length; i++){
-//                     //テキストをlabelタグで囲んだ要素を準備する
-//                     old_node[i] = old_nodes[i].parentNode.childNodes[1];
-//                     new_node[i] = document.createElement('label');
-//                     new_node[i].textContent = old_node[i].textContent
-//                     //いま見ているinputタグに「ID属性+識別子」を付与し、nameと同様の値を振る + labelには同じ値を持つfor属性を付与する
-//                     var input_elm = old_nodes[i].parentNode.childNodes[0]
-//                     var id = input_elm.name + "_" + i;
-//                     input_elm.setAttribute( 'id', id );
-//                     new_node[i].htmlFor = id
-//                     //HTMLで指定のテキスト要素をlabelタグで囲まれた要素に更新する
-//                     target_node[i] = old_node[i].parentNode;
-//                     target_node[i].replaceChild(new_node[i],old_node[i]);
-//                 }
-//             }
-//             setLebelTag("td input[type='radio']")
-//             setLebelTag("td input[type='checkbox']")
+    /*元々のソースコードは、inputタグの選択肢のテキストに<label>タグがないため、特定のCSSが適用しにくい状態。
+      そのため、全てのinputタグの選択肢のテキストを<label>タグで囲う処理を行う */
+    setInputLebelTag("td input[type='radio']");
+    setInputLebelTag("td input[type='checkbox']");
+    function setInputLebelTag(selector_text) {
+        var old_input_nodes = document.querySelectorAll(selector_text);
+        Array.from(old_input_nodes).map(function (old_input_node, index) {
+            //inputタグのテキストをlabelタグで囲んだ要素を準備する
+            var td_tag_elm = old_input_node.parentNode;
+            var input_tag_text = td_tag_elm.childNodes[1]; //inputタグ内のテキスト情報を取得
+            var new_input_node = document.createElement('label')
+            new_input_node.textContent = input_tag_text.textContent //"input_tag_text"だけでは[object Text]が返るので中身を取り出してからlabelで囲う
 
-//             //ボタンの文言の修正
-//             document.getElementsByName("smpSubmit")[0].value = "連絡する"
+            //inputタグのID属性とlabelタグのfor属性に値を付与。値にはinputタグのname属性の値に加え、選択肢を識別するためにindexも使う
+            var input_tag = td_tag_elm.childNodes[0]
+            var tag_label_id = input_tag.name + "_" + index;
+            input_tag.setAttribute('id', tag_label_id);
+            new_input_node.htmlFor = tag_label_id
 
-//             //フォームの読み込み終了フラグ
-//             dataLayer.push({'event': 'smpFormLoaded'});
+            //実際のHTMLで指定のテキスト要素をlabelタグで囲まれた要素に更新する
+            var target_node = input_tag_text.parentNode;
+            target_node.replaceChild(new_input_node, input_tag_text);
+        });
+    }
 
-//             //<--フォームのレイアウト修正
-//             //ループの解除
-//             clearInterval(timer);
+    //フォームの入力サンプルを表示
+    $(function () {
+        sampleInput("[name='Public::EmbeddedApplication::User_D__P__D_email']", "例 - sample@pigeon.com");
+        sampleInput("[name='Public::EmbeddedApplication::User_D__P__D_name1']", "例 - 山田");
+        sampleInput("[name='Public::EmbeddedApplication::User_D__P__D_VisitorData.attribute36']", "ご入力ください");
+        function sampleInput(selector, text) {
+            var elms = document.querySelectorAll(selector);
+            Array.from(elms).map(function (elm) { return elm.placeholder = text });
+        }
+    });
 
-
-//         }
-//     }, 300);
-//     //<--ループ
-
-
-// }
+    //ボタン文言の修正
+    Array.from(document.getElementsByName("smpSubmit")).map(function (elm) { elm.value = "連絡する" })
+}
