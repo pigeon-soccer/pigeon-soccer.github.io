f8ebd6db5e2473e04764f4ff319860d6b1a29c20
masatoEmata <lh57.nm@gmail.com>
2022-10-03 00:43:09 +0900

    deferLoadingJs

--- join/index.php
+++ join/index.php
@@ -74,15 +74,14 @@
   })(window,document,'script','dataLayer','GTM-MBJJ9KL');</script>
   <!-- End Google Tag Manager -->
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet" loading="lazy">
-
   <!-- Vendor CSS Files -->
   <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet" loading="lazy">
 
   <!-- Template Main CSS File -->
   <!-- =======================================================
@@ -94,7 +93,7 @@
   <link href="../assets/css/laura.css" rel="stylesheet">
   <link href="../assets/css/form.css" rel="stylesheet">
   <link href="../assets/css/join.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" loading="lazy"></script>
+  <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 </head>
 
@@ -509,19 +508,19 @@
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="../assets/vendor/purecounter/purecounter_vanilla.js" loading="lazy"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js" loading="lazy"></script>
-  <script src="../assets/vendor/glightbox/js/glightbox.min.js" loading="lazy"></script>
-  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js" loading="lazy"></script>
-  <script src="../assets/vendor/swiper/swiper-bundle.min.js" loading="lazy"></script>
-  <script src="../assets/vendor/waypoints/noframework.waypoints.js" loading="lazy"></script>
-  <script src="../assets/vendor/php-email-form/validate.js" loading="lazy"></script>
-  <script src="../assets/js/form.js" loading="lazy"></script>
-  <script src="https://sdk.form.run/js/v2/formrun.js" loading="lazy"></script>
-
   <!-- Template Main JS File -->
-  <script src="../assets/js/join.js" loading="lazy"></script>
+  <script defer src="../assets/js/join.js" loading="lazy"></script>
+  <script defer src="../assets/js/form.js" loading="lazy"></script>
+  <!-- Vendor JS Files -->
+  <script defer src="../assets/vendor/purecounter/purecounter_vanilla.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/glightbox/js/glightbox.min.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/isotope-layout/isotope.pkgd.min.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/swiper/swiper-bundle.min.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/waypoints/noframework.waypoints.js" loading="lazy"></script>
+  <script defer src="../assets/vendor/php-email-form/validate.js" loading="lazy"></script>
+  <script defer src="https://sdk.form.run/js/v2/formrun.js" loading="lazy"></script>
+
 
 </body>
 
