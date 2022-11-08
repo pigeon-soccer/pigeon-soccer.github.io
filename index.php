529ed4f43e52c948cc3c94d565ac7f0bb5700996
masatoEmata <lh57.nm@gmail.com>
2022-11-08 23:52:42 +0900

    updateAtmosphere

--- index.php
+++ index.php
@@ -68,6 +68,7 @@
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-MBJJ9KL');</script>
   <!-- End Google Tag Manager -->
+  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <script src="/assets/js/main.js"></script>
   <link rel="stylesheet" href="/assets/css/reset.css">
   <link rel="stylesheet" href="/assets/css/main.css">
@@ -94,7 +95,7 @@
         <section class="summary summary-about">
           <h2 class="summary__heading">どんな団体？</h2>
           <div class="summary__content">
-            <div class="summary__txtWrapper">
+            <div class="summary__txtWrapper width550">
               <div class="txtWrapper__txt">
                 <p>障がいのある子どものためのサッカースクール「NPO法人ピジョン」。2010年4月から東京都文京区を拠点に活動する非営利団体です。子どもの本来の姿であるわんぱくな活動の場を提供するとともに、チームスポーツを通して基礎体力はもちろん、思いやりの心、人との繋がりを育んでいきます。こうした活動に込めたピジョンの思い、そして私たち自身について、ご紹介します。</p>
               </div>
@@ -107,7 +108,7 @@
         <section class="summary summary-activity">
           <h2 class="summary__heading">教室で何をする？</h2>
           <div class="summary__content">
-            <div class="summary__txtWrapper">
+            <div class="summary__txtWrapper width550">
               <div class="txtWrapper__txt">
                 <p>子どもたちのもつ心や体の症状の種類とレベルはそれぞれです。私たちの団体にも、発達障害や身体障害に関する様々な症状の生徒がいます（自閉症、ダウン症、脳性まひ、多動症など）。なかなか喋らない子もいれば、お喋りが好きで止まらない子もいます。ボールを送り出すのが精一杯な子もいれば、大人顔負けのシュートを放つ子もいます。そうした中でコーチはもちろん、子どもたちはお互いに尊重し、時に助け合い、時に笑い合います。そんな教室の練習内容や場所・日時をご紹介します。
               </div>
@@ -122,17 +123,65 @@
           <div class="summary__content">
             <div class="summary__txtWrapper">
               <div class="txtWrapper__txt">
-                <p>単調になりがちな基礎練習も、コーチが雰囲気を作ることによって子どもの興味を刺激し、遊びの延長として自然に取り組んでいけるような現場作りをしていきます。教室は隔週で開催しており、練習では諦めずにトライすることを伝えます。SNSではそんな日々の活動の内容や、スタッフの気づきを報告しています。活動の雰囲気をつかみたい方はぜひ、ご覧ください。</p>
+                <p>
+                  現場では、元気いっぱいな生徒の声とスタッフの声が響いています。<br>
+                  人見知りな子も、リレーやパス、試合をするなかで溶け込み、徐々に笑顔を見せてくれます。<br>
+                  本サイトでは、保護者の声や、活動を支えるスタッフの写真・動画、SNSでの報告を掲載。<br>
+                  活動の明るい雰囲気を知ることができるので、ぜひご覧ください。</p>
+              </div>
+              <div class="col-lg-8 d-flex flex-column align-items-stretch">
+                <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+
+                  <div class="row">
+
+                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                      <div class="count-box">
+                        <i class="bi bi-clock" style="color: #5370E0;"></i>
+                        <p class="txtLink"><a href="/activity/#episode" target="_blank">保護者の体験談</a></p>
+                        <p>実際に活動を体験したお子さんの様子を保護者の方々にコメントしていただいた内容を掲載しています。</p>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                      <div class="count-box">
+                        <i class="bi bi-book" style="color: #E05126"></i>
+                        <p class="txtLink"><a href="/activity/#episode" target="_blank">スタッフのアルバム</a></p>
+                        <p>
+                          <i class="bi bi-box-arrow-up-right"></i>
+                          <p>活動を支える現地のスタッフや生徒達の写真掲載しています(生徒の写真はプライバシー保護のためあまり充実させておりません)。</p>
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                      <div class="count-box">
+                        <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
+                        <p class="txtLink"><a href="/activity/#episode" target="_blank">インタビュー動画</a></p>
+                        <p>株式会社IR Robotics様の「Japan Stock Channel」に取材いただいた、代表・副代表理事のインタビュー動画です</p>
+                        <iframe width="344" height="315" src="https://www.youtube.com/embed/QlHb1P33YLU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      </div>
+                    </div>
+                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch height750">
+                      <div class="count-box">
+                        <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
+                        <p class="txtLink"><a href="/activity/#episode" target="_blank">SNS</a></p>
+                        <p>日々の活動の内容やスタッフの気づきを報告しています。</p>
+                        <a class="twitter-timeline" data-height="666" href="https://twitter.com/PigeonSoccerNPO?ref_src=twsrc%5Etfw">Twitter読み込み中...</a>
+                        <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                      </div>
+                    </div>
+                  </div><!-- End row-->
               </div>
             </div>
-            <div class="summary__snsWindow">
-              <a class="twitter-timeline" data-height="666" href="https://twitter.com/PigeonSoccerNPO?ref_src=twsrc%5Etfw">Tweets</a>
-              <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+          <div class="summary__content">
+            <div class="summary__txtWrapper width550">
+
             </div>
+
           </div>
         </section>
 
-            </div>
+
         </section>
   </div><!--commonInner-->
 </main><!-- /main -->
