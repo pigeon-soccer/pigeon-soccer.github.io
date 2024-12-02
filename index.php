17cc93728928fbfa683812416adfa486a9404209
masatoEmata <jng.bosn@gmail.com>
2024-12-02 23:49:36 +0900

    replace youtube iframe to lite-youtube module

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html lang="ja">
+
 <head>
   <meta charset="utf-8">
   <title>東京都の障がい児専門サッカースクール | NPO法人ピジョン公式</title>
   <link rel="canonical" href="https://soccer.pigeon-2ndplace.org/">
-  <meta name="description" content="自閉症・発達障害・知的障害などの子どもたちがサッカーに笑顔でチャレンジし、基礎体力と社会性を育む居場所。東京都文京区を拠点に継続的な活動で自立を支援。保護者・学校と連携した個別支援を提供します。">
+  <meta name="description"
+    content="自閉症・発達障害・知的障害などの子どもたちがサッカーに笑顔でチャレンジし、基礎体力と社会性を育む居場所。東京都文京区を拠点に継続的な活動で自立を支援。保護者・学校と連携した個別支援を提供します。">
   <script type="application/ld+json">
     [
       {
@@ -58,154 +60,171 @@
       }
     ]
   </script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <link rel="icon" sizes="16x16 32x32 48x48 64x64" href="/assets/img/logo/favicon_pigeon.ico"/>
+  <link rel="icon" sizes="16x16 32x32 48x48 64x64" href="/assets/img/logo/favicon_pigeon.ico" />
   <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MBJJ9KL');</script>
+  <script>(function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MBJJ9KL');</script>
   <!-- End Google Tag Manager -->
   <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <script src="/assets/js/main.js"></script>
   <link rel="stylesheet" href="/assets/css/reset.css">
   <link rel="stylesheet" href="/assets/css/main.css">
+  <link rel="stylesheet" href="/assets/vendor/lite-youtube-embed/lite-yt-embed.css">
+  <script src="/assets/vendor/lite-youtube-embed/lite-yt-embed.js"></script>
 </head>
 
 <body id="top">
-<div class="wrap">
-<header id="header">
-</header>
-<main id="index">
-  <div class="mainVisual">
-    <h1 class="mainVisual__txt">
-      <div class="txt__main">ココロをつなぐ<br>パスをする</div>
-      <div class="txt__sub">障がいがあってもなくても、<br>スポーツを通して得られる<br>人との絆と成長があります。</div>
-    </h1>
-    <p class="mainVisual__scroll-down"><a href="#mainContent">Scroll<span></span></a></p>
-    <div class="mainVisual__img"></div>
-  </div><!-- /mainVisual -->
+  <div class="wrap">
+    <header id="header">
+    </header>
+    <main id="index">
+      <div class="mainVisual">
+        <h1 class="mainVisual__txt">
+          <div class="txt__main">ココロをつなぐ<br>パスをする</div>
+          <div class="txt__sub">障がいがあってもなくても、<br>スポーツを通して得られる<br>人との絆と成長があります。</div>
+        </h1>
+        <p class="mainVisual__scroll-down"><a href="#mainContent">Scroll<span></span></a></p>
+        <div class="mainVisual__img"></div>
+      </div><!-- /mainVisual -->
 
-  <div id="mainContent">
-    <section class="commonSection">
-      <div class="commonInner sectionContent">
+      <div id="mainContent">
+        <section class="commonSection">
+          <div class="commonInner sectionContent">
 
-        <section class="summary summary-about">
-          <h2 class="summary__heading">ピジョンとは？</h2>
-          <div class="summary__content">
-            <div class="summary__txtWrapper width550">
-            <h3>仲間と繋がる、安心の居場所</h3>
-              <div class="txtWrapper__txt">
-                <p>
-                  NPO法人ピジョンは、2010年4月から東京都文京区で活動する、障がいのある子どものためのサッカースクールです。
-                  私たちは生徒が安心して通える居場所として存在することで、まず笑顔と自信を得ることを第一目標としています。
-                  その上で、チームスポーツを通じて多様な仲間と経験を積むことで、社交性と基礎体力を育み、自立に向けたステップを踏むことを目的としています。
-                  なぜ、よくあるスポーツクラブや障害児向けのイベントではなく、スクールとして継続支援をしなければならないのでしょうか？
-                  団体概要のページでは、ピジョンの思いと沿革をご紹介しています。
-                </p>
+            <section class="summary summary-about">
+              <h2 class="summary__heading">ピジョンとは？</h2>
+              <div class="summary__content">
+                <div class="summary__txtWrapper width550">
+                  <h3>仲間と繋がる、安心の居場所</h3>
+                  <div class="txtWrapper__txt">
+                    <p>
+                      NPO法人ピジョンは、2010年4月から東京都文京区で活動する、障がいのある子どものためのサッカースクールです。
+                      私たちは生徒が安心して通える居場所として存在することで、まず笑顔と自信を得ることを第一目標としています。
+                      その上で、チームスポーツを通じて多様な仲間と経験を積むことで、社交性と基礎体力を育み、自立に向けたステップを踏むことを目的としています。
+                      なぜ、よくあるスポーツクラブや障害児向けのイベントではなく、スクールとして継続支援をしなければならないのでしょうか？
+                      団体概要のページでは、ピジョンの思いと沿革をご紹介しています。
+                    </p>
+                  </div>
+                  <p class="commonDetailBtn"><a href="/about/">団体の思い、沿革</a></p>
+                </div>
+                <figure class="summary__img"><img src="/assets/img/snapshot/smiling-general-coach.jpg"
+                    alt="優しい笑顔の武藤監督の写真 - 障がい児のためのサッカースクール" loading="lazy" /></figure>
               </div>
-              <p class="commonDetailBtn"><a href="/about/">団体の思い、沿革</a></p>
-            </div>
-            <figure class="summary__img"><img src="/assets/img/snapshot/smiling-general-coach.jpg" alt="優しい笑顔の武藤監督の写真 - 障がい児のためのサッカースクール" loading="lazy"/></figure>
-          </div>
-        </section><!--/summary summary-about-->
+            </section><!--/summary summary-about-->
 
-        <section class="summary summary-activity">
-          <h2 class="summary__heading">教室で何をする？</h2>
-          <div class="summary__content">
-            <div class="summary__txtWrapper width550">
-              <h3>個性が輝く、楽しいサッカー体験</h3>
-              <div class="txtWrapper__txt">
-                <p>
-                  子どもたちの持つ心や体の症状は、一人ひとり異なります。
-                  私たちの教室には、自閉症、ダウン症、発達障害、脳性まひ、多動症など、さまざまな障がいを持つお子さまが参加しています。
-                  なかなか話さない子もいれば、おしゃべりが大好きな子もいます。
-                  ボールを蹴るのが精一杯な子もいれば、大人顔負けのシュートを決める子もいます。
-                  このように個性豊かな生徒が集まる場ですが、コーチたちは継続的に子供たちを支援し、時に保護者と会話することで、一人ひとりの特性に合わせた支援のノウハウを蓄積ながら指導していきます。
-                  生徒たちは、他の生徒や様々な大人と一緒にこの場所でサッカーを楽しみ、その経験を通してお互いを尊重し、理解し、協調し、自己管理することを学びます。
-                  保護者たちは、普段の生活の延長では感じ取りにくい子供たちの成長や変化について、他の暖かい保護者たちと一緒に練習風景を見守るなかで、確かに感じ取ることができます。
-                  活動概要のページでは、そんなピジョンの活動内容や、練習を見守る他の保護者さまの体験談を紹介しています。
-                </p>
+            <section class="summary summary-activity">
+              <h2 class="summary__heading">教室で何をする？</h2>
+              <div class="summary__content">
+                <div class="summary__txtWrapper width550">
+                  <h3>個性が輝く、楽しいサッカー体験</h3>
+                  <div class="txtWrapper__txt">
+                    <p>
+                      子どもたちの持つ心や体の症状は、一人ひとり異なります。
+                      私たちの教室には、自閉症、ダウン症、発達障害、脳性まひ、多動症など、さまざまな障がいを持つお子さまが参加しています。
+                      なかなか話さない子もいれば、おしゃべりが大好きな子もいます。
+                      ボールを蹴るのが精一杯な子もいれば、大人顔負けのシュートを決める子もいます。
+                      このように個性豊かな生徒が集まる場ですが、コーチたちは継続的に子供たちを支援し、時に保護者と会話することで、一人ひとりの特性に合わせた支援のノウハウを蓄積ながら指導していきます。
+                      生徒たちは、他の生徒や様々な大人と一緒にこの場所でサッカーを楽しみ、その経験を通してお互いを尊重し、理解し、協調し、自己管理することを学びます。
+                      保護者たちは、普段の生活の延長では感じ取りにくい子供たちの成長や変化について、他の暖かい保護者たちと一緒に練習風景を見守るなかで、確かに感じ取ることができます。
+                      活動概要のページでは、そんなピジョンの活動内容や、練習を見守る他の保護者さまの体験談を紹介しています。
+                    </p>
+                  </div>
+                  <p class="commonDetailBtn"><a href="/activity/">保護者の体験談、詳しい練習内容</a></p>
+                </div>
+                <figure class="summary__img"><img src="/assets/img/snapshot/heading-practice-throwing-ball-mini.jpg"
+                    alt="生徒とサッカーの練習をする鈴木ゆうきコーチの写真 - 障がい児のためのサッカースクール" loading="lazy" /></figure>
               </div>
-              <p class="commonDetailBtn"><a href="/activity/">保護者の体験談、詳しい練習内容</a></p>
-            </div>
-            <figure class="summary__img"><img src="/assets/img/snapshot/heading-practice-throwing-ball-mini.jpg" alt="生徒とサッカーの練習をする鈴木ゆうきコーチの写真 - 障がい児のためのサッカースクール" loading="lazy"/></figure>
-          </div>
+            </section>
+
+            <section class="summary summary-atomosphere">
+              <h2 class="summary__heading">活動の雰囲気は？</h2>
+              <div class="summary__content">
+                <div class="summary__txtWrapper">
+                  <div class="txtWrapper__txt">
+                    <p>
+                      現場では、元気いっぱいの子どもたちとスタッフの笑顔があふれています。
+                      人見知りのお子さまも、リレーやパス、試合を通して自然と溶け込み、笑顔になっていきます。
+                      保護者の声や活動の様子は、写真・動画やSNSでご覧いただけます。
+                      ぜひ、ピジョンの温かい雰囲気を感じてみてください。
+                    </p>
+                  </div>
+                  <div class="col-lg-8 d-flex flex-column align-items-stretch">
+                    <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+
+                      <div class="row">
+
+                        <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                          <div class="count-box">
+                            <i class="bi bi-clock" style="color: #5370E0;"></i>
+                            <p class="txtLink txtLinkEmphasis"><a href="/activity/#episode" target="_blank">保護者の体験談</a>
+                            </p>
+                            <p>実際に活動を体験したお子さんの様子を保護者の方々にコメントしていただいた内容を掲載しています。</p>
+                          </div>
+                        </div>
+
+                        <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                          <div class="count-box">
+                            <i class="bi bi-book" style="color: #E05126"></i>
+                            <p class="txtLink txtLinkEmphasis"><a href="/join/#portfolio" target="_blank">スタッフのアルバム</a>
+                            </p>
+                            <p>
+                              <i class="bi bi-box-arrow-up-right"></i>
+                            <p>活動を支える現地のスタッフや生徒達の写真掲載しています(生徒の写真はプライバシー保護のためあまり充実させておりません)。</p>
+                            </p>
+                          </div>
+                        </div>
+                        <div class="col-md-6 mt-5 d-md-
+<!-- @import " [TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
+                          flex align-items-md-stretch">
+                          <div class="count-box">
+                            <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
+                            <p class="txtLink txtLinkEmphasis"><a href="https://www.youtube.com/embed/QlHb1P33YLU"
+                                target="_blank">インタビュー動画</a></p>
+                            <p>株式会社IR Robotics様の「Japan Stock Channel」に取材いただいた、代表理事・武藤、副代表・鈴木のインタビュー動画です</p>
+                            <lite-youtube videoid="QlHb1P33YLU" playlabel="Play: Crayon Physics Deluxe - Trailer HD"
+                              style="width:344px;height:315px;"></lite-youtube>
+                          </div>
+                        </div>
+                        <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch height750">
+                          <div class="count-box">
+                            <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
+                            <p class="txtLink txtLinkEmphasis"><a href="https://twitter.com/PigeonSoccerNPO"
+                                target="_blank">SNS</a></p>
+                            <p>日々の活動の内容やスタッフの気づきを報告しています。</p>
+                            <a class="twitter-timeline" data-height="666"
+                              href="https://twitter.com/PigeonSoccerNPO?ref_src=twsrc%5Etfw">Twitter読み込み中...</a>
+                            <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                          </div>
+                        </div>
+                      </div><!-- End row-->
+                    </div>
+                  </div>
+                </div>
+                <div class="summary__content">
+                  <div class="summary__txtWrapper width550">
+
+                  </div>
+
+                </div>
+            </section>
+
+
         </section>
+      </div><!--commonInner-->
+    </main><!-- /main -->
 
-        <section class="summary summary-atomosphere">
-          <h2 class="summary__heading">活動の雰囲気は？</h2>
-          <div class="summary__content">
-            <div class="summary__txtWrapper">
-              <div class="txtWrapper__txt">
-                <p>
-                  現場では、元気いっぱいの子どもたちとスタッフの笑顔があふれています。
-                  人見知りのお子さまも、リレーやパス、試合を通して自然と溶け込み、笑顔になっていきます。
-                  保護者の声や活動の様子は、写真・動画やSNSでご覧いただけます。
-                  ぜひ、ピジョンの温かい雰囲気を感じてみてください。
-                </p>
-              </div>
-              <div class="col-lg-8 d-flex flex-column align-items-stretch">
-                <div class="content ps-lg-4 d-flex flex-column justify-content-center">
-
-                  <div class="row">
-
-                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
-                      <div class="count-box">
-                        <i class="bi bi-clock" style="color: #5370E0;"></i>
-                        <p class="txtLink txtLinkEmphasis"><a href="/activity/#episode" target="_blank">保護者の体験談</a></p>
-                        <p>実際に活動を体験したお子さんの様子を保護者の方々にコメントしていただいた内容を掲載しています。</p>
-                      </div>
-                    </div>
-
-                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
-                      <div class="count-box">
-                        <i class="bi bi-book" style="color: #E05126"></i>
-                        <p class="txtLink txtLinkEmphasis"><a href="/join/#portfolio" target="_blank">スタッフのアルバム</a></p>
-                        <p>
-                          <i class="bi bi-box-arrow-up-right"></i>
-                          <p>活動を支える現地のスタッフや生徒達の写真掲載しています(生徒の写真はプライバシー保護のためあまり充実させておりません)。</p>
-                        </p>
-                      </div>
-                    </div>
-                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
-                      <div class="count-box">
-                        <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
-                        <p class="txtLink txtLinkEmphasis"><a href="https://www.youtube.com/embed/QlHb1P33YLU" target="_blank">インタビュー動画</a></p>
-                        <p>株式会社IR Robotics様の「Japan Stock Channel」に取材いただいた、代表理事・武藤、副代表・鈴木のインタビュー動画です</p>
-                        <iframe width="344" height="315" src="https://www.youtube.com/embed/QlHb1P33YLU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                      </div>
-                    </div>
-                    <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch height750">
-                      <div class="count-box">
-                        <i class="bi bi-geo-alt" style="color: #f3a200;"></i>
-                        <p class="txtLink txtLinkEmphasis"><a href="https://twitter.com/PigeonSoccerNPO" target="_blank">SNS</a></p>
-                        <p>日々の活動の内容やスタッフの気づきを報告しています。</p>
-                        <a class="twitter-timeline" data-height="666" href="https://twitter.com/PigeonSoccerNPO?ref_src=twsrc%5Etfw">Twitter読み込み中...</a>
-                        <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                      </div>
-                    </div>
-                  </div><!-- End row-->
-              </div>
-            </div>
-          </div>
-          <div class="summary__content">
-            <div class="summary__txtWrapper width550">
-
-            </div>
-
-          </div>
-        </section>
-
-
-        </section>
-  </div><!--commonInner-->
-</main><!-- /main -->
-
-<footer id="footer">
-</footer><!-- /footer -->
-</div>
+    <footer id="footer">
+    </footer><!-- /footer -->
+  </div>
 </body>
+
 </html>
